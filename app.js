87f6e9600f9888987d92aeaa2a52a9636acd43e9
vmollineri <92593085+vmollineri@users.noreply.github.com>
2021-10-18 23:57:00 +0200

    Ajout de la fonctionnalité (redemander un calcul)

--- app.js
+++ app.js
@@ -32,40 +32,43 @@ function division(nombreA, nombreB) {
     Fin déclaration des fonctions
 */
 
-let choix;
+let choix, restart = false;
 
 do {
-    choix = Number(prompt('Que souhaitez-vous faire ?\n\n 1 - Addition\n 2 - Multiplication\n 3 - Soustraction\n 4 - Division\n')); 
-} while (choix != 1 && choix != 2 && choix != 3 && choix != 4);
+    do {
+        choix = Number(prompt('Que souhaitez-vous faire ?\n\n 1 - Addition\n 2 - Multiplication\n 3 - Soustraction\n 4 - Division\n')); 
+    } while (choix != 1 && choix != 2 && choix != 3 && choix != 4);
 
 
-let nombreUn, nombreDeux;
+    let nombreUn, nombreDeux;
 
-do {
-    nombreUn = Number(prompt('Veuillez saisir votre premier nombre : '));
-    nombreDeux = Number(prompt('Veuillez saisir votre deuxième nombre : '));
-} while (isNaN(nombreUn) || isNaN(nombreDeux));
+    do {
+        nombreUn = Number(prompt('Veuillez saisir votre premier nombre : '));
+        nombreDeux = Number(prompt('Veuillez saisir votre deuxième nombre : '));
+    } while (isNaN(nombreUn) || isNaN(nombreDeux));
 
 
-try {
-    switch (choix) {
-        case 1:
-            resultat = addition(nombreUn, nombreDeux);
-            break;
-        case 2:
-            resultat = multiplication(nombreUn, nombreDeux);
-            break;
-        case 3:
-            resultat = soustraction(nombreUn, nombreDeux);
-            break;
-        case 4:
-            resultat = division(nombreUn, nombreDeux);
-            break;
-        default:
-            throw new Error('Oups une erreur est survenu !');
+    try {
+        switch (choix) {
+            case 1:
+                resultat = addition(nombreUn, nombreDeux);
+                break;
+            case 2:
+                resultat = multiplication(nombreUn, nombreDeux);
+                break;
+            case 3:
+                resultat = soustraction(nombreUn, nombreDeux);
+                break;
+            case 4:
+                resultat = division(nombreUn, nombreDeux);
+                break;
+            default:
+                throw new Error('Oups une erreur est survenu !');
+        }
+        alert('Voici le résultat de votre opération : ' + resultat);
+
+    } catch (error) {
+        alert(error);
     }
-    alert('Voici le résultat de votre opération : ' + resultat);
-
-} catch (error) {
-    alert(error);
-}
+    restart = confirm('Un autre calcul ?');
+} while (restart);
